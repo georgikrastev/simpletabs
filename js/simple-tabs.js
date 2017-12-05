9ceb663e469d7af71001bd32b68820a8f0608b2f
georgikrastev <georgi.krystev89@gmail.com>
2017-12-05 22:15:19 +0200

    Remove old script, Add more content, Update some styles, Move all script tags at the bottom.

--- js/simple-tabs.js
+++ js/simple-tabs.js
@@ -1,48 +1,104 @@
-// TO DO: callbacks logic
+( function( $ ) {
 
-(function ( $ ) {
-	var methods = {
-		init: function ( options ) {
-			var $this = this;
+	var simpleTabs = function(element, options) {
+		var _this = this;
 
-			var settings = $.extend($.fn.sTabs.defaults, options);
-
-			settings.init.call($this);
-		},
-		show: function( selector ) {
-			// Before Show
-			console.log($.fn.sTabs.defaults)
-
-			console.log('Method show called for selector: ' + selector);
-
-			// After Show
-		},
-		hide: function( selector ) {
-			console.log('Method hide called for selector: ' + selector);
-		}		
+		_this.settings = $.extend($.fn.simpleTabs.defaults, options);
+		_this.element = $(element);
+		
+		_this.init();
 	}
 
-	$.fn.sTabs = function ( options ) {
-		if ( methods[options] ) {
-			return methods[ options ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof options === 'object' || ! options ) {
-			// Default to "init"
-			return methods.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' +  options + ' does not exist!' );
-		}   
+	simpleTabs.prototype.init = function() {
+		var _this = this;
+		var settings = _this.settings;
+		var $tabsContainer = _this.element;
+
+		_this.buildClases($tabsContainer);
+		_this.activateTab();
+
+		// Trigger `onInit` callback function
+		settings.onInit();
 	}
 
-	$.fn.sTabs.defaults = {
-		fade: 						false, // Boolean -- Add/Remove tab transition
+	simpleTabs.prototype.buildClases = function($container) {
+		var _this = this;
+		var fadeClass = _this.settings.animate === true ? 'fade' : '';
+
+		$container
+			.find('.simple-tabs__content')
+				.children()
+				.addClass(fadeClass);
+	}
+
+	simpleTabs.prototype.updateTab = function($activeTabLink, $activeTab) {
+		var _this = this;
+
+		$activeTabLink
+			.parent()
+				.add( $activeTab )
+				.addClass('active')
+				.siblings()
+				.removeClass('active');
+
+		// Trigger `onAfterChange` callback function
+		_this.settings.onAfterChange();
+	}
+
+	simpleTabs.prototype.activateTab = function() {
+		var _this = this;
+		var $tabLink = $('.simple-tabs__nav a');
+
+		$tabLink.on('click', function(event) {
+			event.preventDefault();
+
+			var $this = $(this);
+			var tabID = $this.attr('href');
+			var $activeTab = $(tabID);
+
+			// Trigger `onBeforeChange` callback function
+			_this.settings.onBeforeChange();
+
+			_this.updateTab($this, $activeTab);
+		});
+	}
+
+	simpleTabs.prototype.change = function( selector ) {
+		var _this = this;
+
+		var $activeTabLink = $('[href="' + selector + '"]');
+		var $activeTab = $(selector);
+
+		// Trigger `onBeforeChange` callback function
+		_this.settings.onBeforeChange();
+
+		_this.updateTab($activeTabLink, $activeTab);
+	}
+
+	$.fn.simpleTabs = function(options) {
+		var args = arguments;
+		
+		return this.each(function() {			
+			var $element = $(this);
+
+        	if ($.type(options) === 'string') $element.data('simpletabs')[options](args[1]);
+
+			if( $element.data('simpletabs')) {
+				return;
+			}
+
+			var instance = new simpleTabs(this, options);
+
+			$element.data('simpletabs', instance);
+		});
+	}
+
+	$.fn.simpleTabs.defaults = {
+		animate: 					true, // Boolean -- Add/Remove tab transition
 		defaultTab: 				0, // Int -- Index of the opened tab on page load
-		tabsNavigationSelector: 	'', // String -- Class or ID of the Tabs Navigation container
-		tabsContentSelector: 		'', // String --  Class or ID of the Tabs Content container
-		init: 						function() {},
-		beforeShow: 				function() {},
-		afterShow: 					function() {},
-		beforeClose: 				function() {},
-		afterClose: 				function() {}
+		onInit: 					function() {},
+		onBeforeChange: 			function() {},
+		onAfterChange: 				function() {}
 	}
 
-} ( jQuery ));
+})( jQuery );
